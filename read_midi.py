0ac455fdaca9736e95caaed9c0d289060f300126
Hyun Soo Jeon <hsjeon@stanford.edu>
2021-11-01 15:55:37 -0700

    read_midi.py in progress, plans.txt added for an overview of future steps

--- read_midi.py
+++ read_midi.py
@@ -1,3 +1,4 @@
+# import statements
 from mido import MidiFile
 import pandas as pd
 import numpy as np
@@ -5,17 +6,37 @@ import numpy as np
 import sys
 import pdb
 
+# constants
 MIDI_NOTE_RANGE = 128
+DEFAULT_TICKS_PER_BEAT = 960
+DEFAULT_TIME_SIG_NUM, DEFAULT_TIME_SIG_DENOM = 4, 4
+
 
 def parse_midi_messages(filename):
+    '''
+    Function: parse_midi_messages
+
+    Input
+        filename(str) : name of MIDI file to read
+    Output
+        TBD
+
+    Reads in the provided MIDI file and converts the note arrangements into a NumPy array,
+    where the rows represent note pitches (0-127) and columns represent unit time step (MIDI ticks).
+    At any timestep, pressed notes have values of 1 and others have values of 0.
+    '''
     mid = MidiFile(filename)
+    ticks_per_beat = mid.ticks_per_beat if mid.ticks_per_beat is not None else DEFAULT_TICKS_PER_BEAT
 
     for track_idx, track in enumerate(mid.tracks):
+        # time signature e.g., 3/4, 4/4
+        time_num, time_denom = DEFAULT_TIME_SIG_NUM, DEFAULT_TIME_SIG_DENOM
         msg_list = []
 
         for msg in track:
             if msg.is_meta:
-                continue
+                if msg.type == 'time_signature':
+                    time_num, time_denom = msg.numerator, msg.denominator
             elif msg.type in ['note_on', 'note_off']:
                 try:
                     if msg.type == 'note_off':
@@ -29,6 +50,12 @@ def parse_midi_messages(filename):
 
         msg_df = pd.DataFrame(msg_list, columns=['channel', 'type', 'note', 'time', 'velocity'])
 
+        unit_ticks = ticks_per_beat / (4 / (time_denom/4)) # ticks per 16th note
+
+        ### TODO: change to numpy array of velocities, and squish array into 16th-note lengths per column
+
+
+
         notes_arr = np.zeros((MIDI_NOTE_RANGE, msg_df['time'].sum()))
         last_timestep = 0
         last_notes_on = np.zeros(MIDI_NOTE_RANGE).reshape(-1, 1)
@@ -38,9 +65,11 @@ def parse_midi_messages(filename):
                 msg_info = msg_df.iloc[msg_idx]
                 note, time, velocity = msg_info.loc[['note', 'time', 'velocity']]
                 if velocity == 0:
-                    curr_on_set.remove(note)
+                    if note in curr_on_set: #change
+                        curr_on_set.remove(note)
                 else:
-                    curr_on_set.add(note)
+                    if note not in curr_on_set: #change
+                        curr_on_set.add(note)
                 curr_timestep_on_notes = np.array([int(note in curr_on_set) for note in range(MIDI_NOTE_RANGE)])\
                                          .reshape(-1, 1)
                 notes_arr[:, last_timestep:last_timestep + time] = last_notes_on
@@ -53,8 +82,14 @@ def parse_midi_messages(filename):
         notes_arr[:, last_timestep:] = last_notes_on
 
         print('Track {}'.format(track_idx))
-        print(msg_df)
-        print(notes_arr)
+        #pdb.set_trace()
+        #print(msg_df)
+        #print(notes_arr.shape)
+        #for i in range(len(notes_arr)):
+        #    for j in range(len(notes_arr[0])):
+        #        print(int(notes_arr[i][j]), end=' ')
+        #    print()
+
 
 
 def main():
