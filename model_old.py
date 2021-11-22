4eee5ef3850bb32f88fd5f08a734f0cfd9fa49b0
ubuntu <ubuntu@ec2-54-244-167-33.us-west-2.compute.amazonaws.com.internal>
2021-11-22 08:59:02 +0000

    model change

--- model_old.py
+++ model_old.py
@@ -26,11 +26,11 @@ class LSTMModel(object):
     def __init__(self, note_size, in_size, out_size):
         # define model
         self.model = Sequential()
-        self.model.add(LSTM(64, input_shape=(in_size, note_size), return_sequences=True))
+        #self.model.add(LSTM(256, input_shape=(in_size, 1), return_sequences=True))
+        #self.model.add(Dropout(0.3))
+        self.model.add(LSTM(64, input_shape=(note_size, in_size), return_sequences=True))
         self.model.add(Dropout(0.5))
-        self.model.add(LSTM(128, return_sequences=True))
-        self.model.add(Dropout(0.5))
-        self.model.add(LSTM(64))
+        self.model.add(LSTM(64, input_shape=(note_size, in_size)))
         self.model.add(Dropout(0.5))
         self.model.add(Dense(out_size))
         # self.model.add(Dense(out_size, activation='sigmoid'))
@@ -81,9 +81,6 @@ def test(dir_path, input_window_size, step, thresholds):
 
     num_examples, output_size = X.shape[0], X.shape[1]
 
-    # transpose NOTES_SIZE x input_window_size
-    X, Y = np.swapaxes(X, 1, 2), np.swapaxes(Y, 1, 2)
-
     # set a random example as the seed input for music generation later
     seed_idx = np.random.randint(num_examples)
     X_train, X_seed = np.delete(X, seed_idx, axis=0), X[seed_idx, :, :]
@@ -96,7 +93,7 @@ def test(dir_path, input_window_size, step, thresholds):
 
     # music generation!
     gen_epoch = 128
-    pred_result = np.zeros((output_window_size, output_size))
+    pred_result = np.zeros((output_size, output_window_size))
     # pattern will represent the last in_size 16th notes seen
     pattern = X_seed
     out_size = 1
@@ -107,20 +104,20 @@ def test(dir_path, input_window_size, step, thresholds):
             # x = np.reshape(pattern, (1, in_size, 1))
             # predict the next out_size 16th notes from the pattern
             # pred = generator.predict(x).reshape(1, out_size, 1)
-            pred = generator.predict(pattern.reshape(1, input_window_size, output_size)).reshape(output_window_size, output_size)
+            pred = generator.predict(pattern.reshape(1, output_size, input_window_size)).reshape(output_size, output_window_size)
             bool_pred = np.array((pred > threshold).astype(int))
             # convert to string representation
             # pred_str = dc.dataset_to_str(pred)
             # append the new output, and remove the equivalent amount of input from the start for the next prediction
-            pattern = np.concatenate((pattern, bool_pred), axis=0)
-            pattern = pattern[output_window_size:, :]
-            pred_result = np.concatenate((pred_result, bool_pred), axis=0)
+            pattern = np.concatenate((pattern, bool_pred), axis=1)
+            pattern = pattern[:, output_window_size:]
+            pred_result = np.concatenate((pred_result, bool_pred), axis=1)
 
         print(pred_result)
 
         # pred_file = dc.str_to_midi(pred_result, filename='pred_output.mid')
-        pred_file = arr_to_midi(pred_result[1:, :].T, \
-                                filename=('real_new_output/pred_output_' + str(input_window_size) + '_' + str(step) + '_' + str(threshold) + '.mid'))
+        pred_file = arr_to_midi(pred_result[:, 1:], \
+                                filename=('pred_output_' + str(input_window_size) + '_' + str(step) + '_' + str(threshold) + '.mid'))
 
         print('The generated music is at: {}'.format(pred_file))
 
@@ -135,19 +132,21 @@ def main():
     dir_path = sys.argv[1]
 
 
-    for input_window_size in [16]: #, 32, 64]:
+    for input_window_size in [64, 128]:
+    #for input_window_size in [8, 16, 32, 64]:
         plt.figure()
         plt.title('Losses by Step Size: Input Timestep = ' + str(input_window_size))
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
-        for step in (np.array([.5, 1]) * input_window_size).astype(int):
+        #plt.ylim(0, 0.5)
+        for step in [8, 16]:
         #for step in [1, 2, 4, 8]:
-            thresholds = [0.04] #[0.03, 0.04, 0.05, 0.06]
+            thresholds = [0.03, 0.035, 0.04]
             #thresholds = [0.01, 0.015, 0.02, 0.025, 0.03]
             history = test(dir_path, input_window_size, step, thresholds)
             plt.plot(history.history['loss'], label=('step=' + str(step)))
         plt.legend(loc='upper right')
-        plt.savefig('real_new_output/losses_input' + str(input_window_size) + '.png')
+        plt.savefig('losses_input' + str(input_window_size) + '.png')
 
 
 if __name__ == '__main__':
